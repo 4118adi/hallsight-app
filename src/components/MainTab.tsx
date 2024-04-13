9ed5092ca05c4450c441b4ec1f22c96282346b0e
skr <shubhamroy483@gmail.com>
2024-04-14 02:08:01 +0530

    modified tab width

--- src/components/MainTab.tsx
+++ src/components/MainTab.tsx
@@ -5,13 +5,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 type Props = {}
 
 export default function MainTab({ }: Props) {
-    const isThirdTabEnabled = true;
+    const isThirdTabEnabled = false;
     return (
 
         <div className='flex w-full px-0 justify-center'>
-            <Tabs defaultValue="events" className='w-auto'>
+            <Tabs defaultValue="events" className='w-[350px]'>
                 <div className='flex justify-center'>
-                    <TabsList className='flex overflow-hidden w-full px-3'>
+                    <TabsList className='flex overflow-hidden w-auto px-3'>
                         <TabsTrigger className='px-5 overflow-hidden' value="events">Events</TabsTrigger>
                         {isThirdTabEnabled && (
                             <TabsTrigger className=' overflow-hidden px-3' value="dashboard">Dashboard</TabsTrigger>
